7b83d11490b7e576089186aef6d2a0ae2e1f45ff
nhadler <nicholas.hadler@gmail.com>
2018-01-03 12:07:01 -0500

    changed lock wall location

--- .config/i3/i3lock-custom.sh
+++ .config/i3/i3lock-custom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-i3lock -i ~/Pictures/fish.jpg -k --timestr="%I:%M %p" --timesize=32 --timepos="x-90:h-ch+30" --indpos="x+220:h-70" --radius=20 --ring-width=4 --veriftext="" --wrongtext="" --ringvercolor=00000000 --ringwrongcolor=00000000
+i3lock -i ~/Pictures/wallpapers/fish.jpg -k --timestr="%I:%M %p" --timesize=32 --timepos="x-90:h-ch+30" --indpos="x+220:h-70" --radius=20 --ring-width=4 --veriftext="" --wrongtext="" --ringvercolor=00000000 --ringwrongcolor=00000000
