08ba293bc65e90eb22d67e3a3483a4b62d021c45
nhadler <nicholas.hadler@gmail.com>
2018-02-02 21:07:09 -0500

    added multimonitor support

--- .config/polybar/polybar_launch.sh
+++ .config/polybar/polybar_launch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash 
-
-polybar dock
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload dock &
+  done
+else
+  polybar --reload dock &
+fi
