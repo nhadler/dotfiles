c8f09b67651c161596cd8c907301c686dcb9cff0
nhadler <nicholas.hadler@gmail.com>
2018-02-03 11:53:46 -0500

    fixed monitor relaunch

--- .config/polybar/polybar_launch.sh
+++ .config/polybar/polybar_launch.sh
@@ -1,8 +1,2 @@
 #!/bin/bash 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload dock &
-  done
-else
-  polybar --reload dock &
-fi
+polybar --reload dock 
